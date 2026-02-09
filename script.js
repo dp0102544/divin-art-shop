8838bdeae449e4770d4eb2cb525cebaa81cb5888
dp0102544 <dp0102544@gmail.com>
2026-02-09 15:35:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ let cartCount = 0;
 
 function addToCart(productName) {
     cartCount++;
+
+    // update cart count
     document.getElementById("cart-count").innerText = cartCount;
-    alert(productName + " added to cart 🛒");
+
+    // add item to cart list
+    const li = document.createElement("li");
+    li.innerText = productName;
+
+    document.getElementById("cart-items").appendChild(li);
 }
